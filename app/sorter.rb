cd2f35783a339bfebde01074e11c90f156831977
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2021-12-14 19:55:18 +0100

    sorter implementation unique views

--- app/sorter.rb
+++ app/sorter.rb
@@ -1,3 +1,5 @@
+require "set"
+
 class Sorter
   def initialize(input, output)
     @input = input
@@ -14,6 +16,23 @@ class Sorter
 
     result.sort_by { |k, v| -v }.each do |page, views_count|
       @output.puts "#{page} #{views_count} visits"
+    end
+  end
+
+  def sort_by_unique_views
+    result = Hash.new do |hash, page|
+      hash[page] = Set.new
+    end
+
+    @input.each_line do |line|
+      page, ip = line.split
+      result[page] << ip
+    end
+
+    result.transform_values! { |ips| ips.size }
+
+    result.sort_by { |page, views_count| -views_count }.each do |page, views_count|
+      @output.puts "#{page} #{views_count} unique views"
     end
   end
 end
