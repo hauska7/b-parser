77c9dbccaf1dcf288d95f60399f21cb6194dbf18
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2021-12-14 19:47:05 +0100

    unit test for input get_mode for unique_views

--- spec/unit/input_spec.rb
+++ spec/unit/input_spec.rb
@@ -11,6 +11,16 @@ describe Input do
         expect(result).to eq "most_views"
       end
     end
+
+    context "when -unique_views is an argument" do
+      let(:arguments) { ["-unique_views"] }
+
+      it "mode is unique_views" do
+        result = Input.new(arguments).get_mode
+
+        expect(result).to eq "unique_views"
+      end
+    end
   end
 
   describe "clean_arguments_for_file_read!" do
