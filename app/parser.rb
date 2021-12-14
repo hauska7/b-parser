736e5e655ee340896a227d22349ce29ba211f6b3
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2021-12-14 18:32:43 +0100

    integrate input get_mode into parser

--- app/parser.rb
+++ app/parser.rb
@@ -1,1 +1,15 @@
-puts "Hello"
+require_relative "input"
+
+mode = Input.new.get_mode
+
+unless mode
+  puts "Usage:"
+  puts "ruby app/parser.rb webserver.log -most_views"
+  puts "ruby app/parser.rb webserver.log -unique_views"
+  exit 1
+end
+
+case mode
+when "most_views"
+end
+
