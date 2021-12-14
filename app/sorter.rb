69081957c7f84249c6365c9603f6540b132cf47a
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2021-12-14 19:29:26 +0100

    implementation sorter sort by most viewed

---
+++ app/sorter.rb
@@ -0,0 +1,19 @@
+class Sorter
+  def initialize(input, output = nil)
+    @input = input
+    @output = output || $stdout
+  end
+
+  def sort_by_most_views
+    result = {}
+    result.default = 0
+    @input.each_line do |line|
+      page, ip = line.split
+      result[page] = result[page] + 1
+    end
+
+    result.sort_by { |k, v| -v }.each do |page, views_count|
+      @output.puts "#{page} #{views_count} visits"
+    end
+  end
+end
